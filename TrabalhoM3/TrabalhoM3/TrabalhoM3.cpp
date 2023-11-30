3a40c64b44c2f5fe175b7ff79781db9dc662daa0
Marco Antonio Martins Akerman <marco.akerman@edu.univali.br>
2023-11-30 12:27:07 -0300

    falta melhor e pior caso

--- TrabalhoM3/TrabalhoM3/TrabalhoM3.cpp
+++ TrabalhoM3/TrabalhoM3/TrabalhoM3.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include "shellsort.hpp"
 #include "quicksort.hpp"
+#include <chrono>
+
+#define TAM1 5000
+#define TAM2 10000
+#define TAM3 15000
 
 using namespace std;
 
 int main(){
     srand(time(NULL));
-    int vetor[15];
 
-    preencher_vetor(vetor, 15);
+    int count = 0;
+    while(count < 20){
+    int vetor[TAM1];
+
+
+    preencher_vetor(vetor, TAM1); //gera elementos aleatorios
     cout << "Vetor: ";
-    mostrar_vetor(vetor, 15);
-    shellsort2(vetor, 15);
+    mostrar_vetor(vetor, TAM1);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    shellsort(vetor, TAM1);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     cout << endl << "Vetor Shellsort: ";
-    mostrar_vetor(vetor, 15);
+    //mostrar_vetor(vetor, TAM1);
     cout << endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[microSegundos]" << std::endl;
 
+     cout << "Vetor: ";
+     mostrar_vetor(vetor, TAM1);
 
-    
+    }
+    // cout << "endl";
 
+    // quicks(vetor, 0, TAM1-1, 1);
 
-
-
-
-    srand(time(NULL));
-    int vetor[15];
-    int pivo = escolherPivo(vetor, 0, 14);
-
-    preencher_vetor(vetor, 15);
-    cout << "Vetor: ";
-
-    int pivo = escolherPivo(vetor, 0, 14);
-    mostrar_vetor(vetor, 15), pivo;
-
-    quicksort(vetor, 0, 14, pivo);
-
-    cout << endl << "Vetor: ";
-    mostrar_vetor(vetor, 15);
-    cout << endl;
+    // cout << endl << "Vetor Quicksort: ";
+    // mostrar_vetor(vetor, TAM1);
+    // cout << endl;
 }
--- TrabalhoM3/TrabalhoM3/quicksort.hpp
+++ TrabalhoM3/TrabalhoM3/quicksort.hpp
@@ -3,53 +3,21 @@
 #include <time.h>
 using namespace std;
 
-void preencher_vetor( int v[], int tam, int limite=100 ){
-    for( int i=0; i<tam; i++ ){
-        v[i] = rand() % limite;
-    }
-}
-
-void mostrar_vetor( int v[], int tam ){
-    for( int i=0; i<tam; i++ ){
-        cout << v[i] << "  ";
-    }
-}
-
-int escolherPivo(int v[], int esq, int dir, int x = NULL){
-    int escolha;
-    int pivo;
-    int x;
-
-    cout << "escolha o pivo: " << endl;
-    cout << "[1] primeiro elemento" << endl;
-    cout << "[2] último elemento" << endl;
-    cout << "[3] elemento do meio" << endl;
-    cout << "[4] mediana entre o primeiro, o último e o elemento do meio;" << endl;
-    cin >> escolha;
-
-    switch (escolha){
-        case 1:
-            x = v[0];
-        case 2:
-            x = v[dir];
-        case 3:
-            x = v[(esq+dir)/2];
-        case 4:
-            x = v[0];
-        default:
-            cout <<"Valor invalido" << endl;
-            escolherPivo(v, esq, dir);
-    } 
-    return pivo;
-}
-
-
-
-
-void quicksort( int v[], int esq, int dir , int x){
+void quicksortMediana2( int v[], int esq, int dir){
     int aux;
     int i = esq;
     int j = dir;
+    int x;
+    int y = v[(esq+dir)/2];
+    
+    if(v[esq] >= v[dir] && v[dir] <= y){
+        x = v[dir];
+    }else if(v[dir] >= v[esq] && v[esq] <= y){
+        x = v[esq];
+    }else{
+        x = y;
+    }
+
 
     do{
         while( x>v[i] ) i++;
@@ -62,6 +30,99 @@ void quicksort( int v[], int esq, int dir , int x){
             j--;
         }
     }while( i <= j );
-    if( esq < j ) quicksort(v, esq, j, x);
-    if( dir > i ) quicksort(v, i, dir, x);
+    if( esq < j ) quicksortMediana2(v, esq, j);
+    if( dir > i ) quicksortMediana2(v, i, dir);
+}
+
+void quicksortFinal( int v[], int esq, int dir){
+    int aux;
+    int i = esq;
+    int j = dir;
+    int x = v[dir];
+
+    do{
+        while( x>v[i] ) i++;
+        while( x<v[j] ) j--;
+        if( i<= j ){
+            aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++;
+            j--;
+        }
+    }while( i <= j );
+    if( esq < j ) quicksortFinal(v, esq, j);
+    if( dir > i ) quicksortFinal(v, i, dir);
+}
+
+
+void quicksortInicio( int v[], int esq, int dir){
+    int aux;
+    int i = esq;
+    int j = dir;
+    int x = v[esq];
+
+    do{
+        while( x>v[i] ) i++;
+        while( x<v[j] ) j--;
+        if( i<= j ){
+            aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++;
+            j--;
+        }
+    }while( i <= j );
+    if( esq < j ) quicksortInicio(v, esq, j);
+    if( dir > i ) quicksortInicio(v, i, dir);
+}
+
+
+void quicksortMediana1( int v[], int esq, int dir){
+    int aux;
+    int i = esq;
+    int j = dir;
+    int x = v[(esq+dir)/2];
+
+    do{
+        while( x>v[i] ) i++;
+        while( x<v[j] ) j--;
+        if( i<= j ){
+            aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++;
+            j--;
+        }
+    }while( i <= j );
+    if( esq < j ) quicksortMediana1(v, esq, j);
+    if( dir > i ) quicksortMediana1(v, i, dir);
+}
+
+
+
+void quicks(int v[], int esq, int dir, int escolha = 1){
+    int x;
+
+    switch (escolha){
+        case 1:
+            quicksortInicio(v, esq, dir); // primeiro
+        break;
+
+        case 2:
+            quicksortFinal(v, esq, dir); // ultimo
+        break;
+
+        case 3:
+            quicksortMediana1(v, esq, dir); // mediana
+        break;
+
+        case 4:
+            quicksortMediana2(v, esq, dir); // mediana dos 3
+        break;
+
+        default:
+            quicksortMediana1(v, esq, dir);
+        break;
+    }
 }
